3c20314e374d2ce1865a64460351eb93e0c40a14
Michael Frohberg <michaelfrohberg@fullsail.edu>
2012-10-09 18:08:02 -0400

    Revert "Array Property"
    This reverts commit 4f1ff7fb168c2cd74047a64891423bb4256f48ab.

--- sdi-project3/frohberg_michael_assignment3.js
+++ sdi-project3/frohberg_michael_assignment3.js
@@ -3,11 +3,10 @@
 // Project 3
 
 var json = [
-		usedCars = {
-			"make":         "Toyota",
-			"model":         "Prius",
-			"year":             2007,
-			"isEfficient":      true,
-			"gasMileage": ["51mpg", "45mpg", "38mpg"]
-		}	
+	usedCars = {
+		"make":         "Toyota",
+		"model":         "Prius",
+		"year":             2007,
+		"isEfficient":      true	
+	},
 ]
