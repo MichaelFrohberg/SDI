4f1ff7fb168c2cd74047a64891423bb4256f48ab
Michael Frohberg <michaelfrohberg@fullsail.edu>
2012-10-09 18:06:21 -0400

    Array Property
    added array property to object within json

--- sdi-project3/frohberg_michael_assignment3.js
+++ sdi-project3/frohberg_michael_assignment3.js
@@ -3,10 +3,11 @@
 // Project 3
 
 var json = [
-	usedCars = {
-		"make":         "Toyota",
-		"model":         "Prius",
-		"year":             2007,
-		"isEfficient":      true	
-	},
+		usedCars = {
+			"make":         "Toyota",
+			"model":         "Prius",
+			"year":             2007,
+			"isEfficient":      true,
+			"gasMileage": ["51mpg", "45mpg", "38mpg"]
+		}	
 ]
