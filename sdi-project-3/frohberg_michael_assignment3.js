9ccd07cb4b82a746a5104d28a730ce5e1d9d1cd5
Michael Frohberg <michaelfrohberg@fullsail.edu>
2012-10-09 19:22:59 -0400

    Resubmitted
    Accidentally deleted code

---
+++ sdi-project-3/frohberg_michael_assignment3.js
@@ -0,0 +1,13 @@
+// Michael C. Frohberg
+// SDI 1210
+// Project 3
+// Objects and Flowchart
+
+var json = [
+	usedCars = {
+		"make":         "Toyota",
+		"model":         "Prius",
+		"year":             2007,
+		"isEfficient":      true	
+	},
+]
