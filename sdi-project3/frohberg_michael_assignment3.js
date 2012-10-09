08f979d3e6bb9e0ad6ba13338a7212ac41b980ec
Michael Frohberg <michaelfrohberg@fullsail.edu>
2012-10-09 17:44:24 -0400

    my json data
    created usedCar object
    has 2 string properties
    has number property
    has boolean property

---
+++ sdi-project3/frohberg_michael_assignment3.js
@@ -0,0 +1,12 @@
+// Michael C. Frohberg
+// SDI 1210
+// Project 3
+
+var json = [
+	usedCars = {
+		"make":         "Toyota",
+		"model":         "Prius",
+		"year":             2007,
+		"isEfficient":      true	
+	},
+]
