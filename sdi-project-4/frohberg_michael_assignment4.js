7922d94568f8bee487ab7c563086b94262760475
Michael Frohberg <michaelfrohberg@fullsail.edu>
2012-10-15 20:49:14 -0400

    Create Function
    I am trying to tackle the valid phone number format function.
    Creating a function that first determines if there are 12 characters
    then searches for the "-" symbol at the 4 and 8 charAt to verify
    validity.

--- sdi-project-4/frohberg_michael_assignment4.js
+++ sdi-project-4/frohberg_michael_assignment4.js
@@ -2,3 +2,20 @@
 // SDI 1210
 // Project 4
 // Libraries
+
+//Strings:
+// Does a string follow a 123-456-7890 pattern like a phone number?
+
+var isValidNumber = function (phoneNumber) { 
+		if (phoneNumber.length === 12) {
+			if (phoneNumber.indexOf("-") === phoneNumber.charAt(3)) {
+				if (phoneNumber.lastIndexOf("-") === phoneNumber.charAt(7)) {
+					return console.log("You have a valid phone number pattern.");
+					}
+				} else {
+					return console.log("You have an invalid phone number pattern.");
+					}
+		}
+};
+isValidNumber("123-456-7890");
+
