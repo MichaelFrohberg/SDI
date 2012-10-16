636aa64c753cd1b12894094edba1f4dc5dfdc546
Michael Frohberg <michaelfrohberg@fullsail.edu>
2012-10-16 01:58:16 -0400

    Created Library Structure. Used Some Regular Expression
    Created myLibrary as a function.
    Changed isValidNumber function to use regex.
    Returned an Object/Method called isValidNumber.
    Created Global Variable from myLibrary called newLib.

--- sdi-project-4/frohberg_michael_assignment4.js
+++ sdi-project-4/frohberg_michael_assignment4.js
@@ -3,19 +3,50 @@
 // Project 4
 // Libraries
 
-//Strings:
-// Does a string follow a 123-456-7890 pattern like a phone number?
-
-var isValidNumber = function (phoneNumber) { 
-		if (phoneNumber.length === 12) {
-			if (phoneNumber.indexOf("-") === phoneNumber.charAt(3)) {
-				if (phoneNumber.lastIndexOf("-") === phoneNumber.charAt(7)) {
-					return console.log("You have a valid phone number pattern.");
-					}
-				} else {
-					return console.log("You have an invalid phone number pattern.");
-					}
+var myLibrary = function(){
+		/* I  am using regex to determine proper phone format(###-###-####) 
+		/\d{3}: means the first 3 values are digits [0-9]
+		([-]): the "-" in brackets indicates the next value is a dash.
+		The surrounding parentheses 'remembers' the usage of the dash.
+		Followed by another \d{3}, (3 more digits) then "\1" recalls a second dash.
+		Finally ending with \d{4} indicating the final 4 digits. */
+	
+	var regex =  /\d{3}([-])\d{3}\1\d{4}/; 
+		function isValidNumber(p){
+			var OK = regex.exec(p);
+			if (!OK) {
+				return false;
+			} else {
+				return true;
+			}
 		}
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return {
+		"isValidNumber": isValidNumber
+	}
+
 };
-isValidNumber("123-456-7890");
+
+
+
+
+
+
+var newLib = new myLibrary();
+
+console.log("Is 347-525-4218 in valid phone number format? " +
+						newLib.isValidNumber("347-525-4218"));
+console.log("Is 1234567890 in valid phone number format? " +
+						newLib.isValidNumber("1234567890"));
+
 
